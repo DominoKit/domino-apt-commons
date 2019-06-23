9c565d43baacd08329bff465725d4a04701ccbd4
vegegoku <akabme@gmail.com>
2019-06-23 10:48:35 +0300

    add more exception logging methods

--- src/main/java/org/dominokit/domino/apt/commons/ExceptionUtil.java
+++ src/main/java/org/dominokit/domino/apt/commons/ExceptionUtil.java
@@ -1,6 +1,8 @@
 package org.dominokit.domino.apt.commons;
 
 import javax.annotation.processing.Messager;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
 import javax.tools.Diagnostic;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -16,6 +18,31 @@ public class ExceptionUtil {
         StringWriter out = new StringWriter();
         e.printStackTrace(new PrintWriter(out));
         messager.printMessage(Diagnostic.Kind.ERROR, "error while creating source file " + out.getBuffer().toString());
+    }
+
+    /**
+     *
+     * @param messager the messager to print the exception stack trace
+     * @param e exception to be printed
+     * @param element the element producing the error
+     */
+    public static void messageStackTrace(Messager messager, Exception e, Element element) {
+        StringWriter out = new StringWriter();
+        e.printStackTrace(new PrintWriter(out));
+        messager.printMessage(Diagnostic.Kind.ERROR, "error while creating source file " + out.getBuffer().toString(), element);
+    }
+
+    /**
+     *
+     * @param messager the messager to print the exception stack trace
+     * @param e exception to be printed
+     * @param element the element producing the error
+     * @param annotationMirror the annotation producing the error
+     */
+    public static void messageStackTrace(Messager messager, Exception e, Element element, AnnotationMirror annotationMirror) {
+        StringWriter out = new StringWriter();
+        e.printStackTrace(new PrintWriter(out));
+        messager.printMessage(Diagnostic.Kind.ERROR, "error while creating source file " + out.getBuffer().toString(), element, annotationMirror);
     }
 
 }
