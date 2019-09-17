044139a3112cf9873df277b4f7f198b22d1859ee
Ahmad Bawaneh <ahmad.bawaneh@progressoft.com>
2019-09-17 11:19:56 +0300

    add more utility method to processorUtil

--- src/main/java/org/dominokit/domino/apt/commons/ProcessorUtil.java
+++ src/main/java/org/dominokit/domino/apt/commons/ProcessorUtil.java
@@ -1,22 +1,30 @@
 package org.dominokit.domino.apt.commons;
 
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.TypeName;
+
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.*;
+import javax.lang.model.type.ArrayType;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
+import javax.lang.model.util.SimpleTypeVisitor8;
 import javax.lang.model.util.Types;
 import java.lang.annotation.Annotation;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
 public class ProcessorUtil {
+    private static final int FIRST_ARGUMENT = 0;
+    private static final int SECOND_ARGUMENT = 1;
 
     protected final Messager messager;
     protected final Filer filer;
@@ -165,4 +173,179 @@ public class ProcessorUtil {
         TypeMirror stringType = elements.getTypeElement("java.lang.String").asType();
         return types.isAssignable(stringType, typeMirror);
     }
+
+    /**
+     * <p>wrapperType.</p>
+     *
+     * @param type a {@link TypeMirror} object.
+     * @return a {@link TypeName} object.
+     */
+    public TypeName wrapperType(TypeMirror type) {
+        if (isPrimitive(type)) {
+            if ("boolean".equals(type.toString())) {
+                return TypeName.get(Boolean.class);
+            } else if ("byte".equals(type.toString())) {
+                return TypeName.get(Byte.class);
+            } else if ("short".equals(type.toString())) {
+                return TypeName.get(Short.class);
+            } else if ("int".equals(type.toString())) {
+                return TypeName.get(Integer.class);
+            } else if ("long".equals(type.toString())) {
+                return TypeName.get(Long.class);
+            } else if ("char".equals(type.toString())) {
+                return TypeName.get(Character.class);
+            } else if ("float".equals(type.toString())) {
+                return TypeName.get(Float.class);
+            } else if ("double".equals(type.toString())) {
+                return TypeName.get(Double.class);
+            } else {
+                return TypeName.get(Void.class);
+            }
+        } else {
+            return TypeName.get(type);
+        }
+    }
+
+    public boolean isPrimitive(TypeMirror typeMirror) {
+        return typeMirror.getKind().isPrimitive();
+    }
+
+    /**
+     * <p>isPrimitiveArray.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a boolean.
+     */
+    public boolean isPrimitiveArray(TypeMirror typeMirror) {
+        return (isArray(typeMirror) && isPrimitive(arrayComponentType(typeMirror))) || isPrimitive2dArray(typeMirror);
+    }
+
+    private boolean isPrimitive2dArray(TypeMirror typeMirror) {
+        return is2dArray(typeMirror) && isPrimitiveArray(arrayComponentType(typeMirror));
+    }
+
+    /**
+     * <p>isArray.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a boolean.
+     */
+    public boolean isArray(TypeMirror typeMirror) {
+        return TypeKind.ARRAY.compareTo(typeMirror.getKind()) == 0;
+    }
+
+    /**
+     * <p>is2dArray.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a boolean.
+     */
+    public boolean is2dArray(TypeMirror typeMirror) {
+        return isArray(typeMirror) && isArray(arrayComponentType(typeMirror));
+    }
+
+    /**
+     * <p>arrayComponentType.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a {@link TypeMirror} object.
+     */
+    public TypeMirror arrayComponentType(TypeMirror typeMirror) {
+        return ((ArrayType) typeMirror).getComponentType();
+    }
+
+    /**
+     * <p>deepArrayComponentType.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a {@link TypeMirror} object.
+     */
+    public TypeMirror deepArrayComponentType(TypeMirror typeMirror) {
+        TypeMirror type = ((ArrayType) typeMirror).getComponentType();
+        return isArray(type) ? arrayComponentType(type) : type;
+    }
+
+    /**
+     * <p>isEnum.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a boolean.
+     */
+    public boolean isEnum(TypeMirror typeMirror) {
+        return !isNull(types.asElement(typeMirror))
+                && !isPrimitive(typeMirror)
+                && !isPrimitiveArray(typeMirror)
+                && ElementKind.ENUM.compareTo(types.asElement(typeMirror).getKind()) == 0;
+    }
+
+    /**
+     * <p>isCollection.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a boolean.
+     */
+    public boolean isCollection(TypeMirror typeMirror) {
+        return !isPrimitive(typeMirror) && isAssignableFrom(typeMirror, Collection.class);
+    }
+
+    /**
+     * <p>isIterable.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a boolean.
+     */
+    public boolean isIterable(TypeMirror typeMirror) {
+        return !isPrimitive(typeMirror) && isAssignableFrom(typeMirror, Iterable.class);
+    }
+
+    /**
+     * <p>isMap.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a boolean.
+     */
+    public boolean isMap(TypeMirror typeMirror) {
+        return !isPrimitive(typeMirror) && isAssignableFrom(typeMirror, Map.class);
+    }
+
+    /**
+     * <p>firstTypeArgument.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a {@link TypeMirror} object.
+     */
+    public TypeMirror firstTypeArgument(TypeMirror typeMirror) {
+        return ((DeclaredType) typeMirror).getTypeArguments().get(FIRST_ARGUMENT);
+    }
+
+    /**
+     * <p>secondTypeArgument.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a {@link TypeMirror} object.
+     */
+    public TypeMirror secondTypeArgument(TypeMirror typeMirror) {
+        return ((DeclaredType) typeMirror).getTypeArguments().get(SECOND_ARGUMENT);
+    }
+
+    /**
+     * <p>getPackage.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a {@link String} object.
+     */
+    public String getPackage(TypeMirror typeMirror) {
+        return elements.getPackageOf(types.asElement(typeMirror)).getSimpleName().toString();
+    }
+
+    /**
+     * <p>simpleName.</p>
+     *
+     * @param typeMirror a {@link TypeMirror} object.
+     * @return a {@link Name} object.
+     */
+    public Name simpleName(TypeMirror typeMirror) {
+        return types.asElement(typeMirror).getSimpleName();
+    }
+
 }
